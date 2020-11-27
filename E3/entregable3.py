9f7772c06462f7a12d21e3e5069157ce4c4c7b2c
mi7ai <hottmayer@gmail.com>
2020-11-27 20:41:35 +0000

    added Utils

--- E3/entregable3.py
+++ E3/entregable3.py
@@ -1,8 +1,7 @@
 import sys
 from typing import *
-import fileinput
+from Utils import bt_scheme
 
-# from bt_scheme import *
 Pos = Tuple[int, int]
 
 
@@ -53,9 +52,37 @@ def read_level(puzle_lines: List[str]) -> Tuple[List[str], Pos, List[Pos], List[
     return level_map, player_pos, boxes_start, boxes_end
 
 
+# comprobar fuera el numero de mov con la longitud de la lista
+def puzzle_solve():
+    class PuzzlePS(PartialSolution):
+        def __init__(self, ds, mapa, pos_jugador):
+            self.ds = ds
+            self.n = len(self.ds)
+
+        def is_solution(self) -> bool:
+            pass
+
+        def get_solution(self) -> Solution:
+            return self.ds
+
+        # def f(self) -> Union[int, float]:
+        #     pass
+        #
+        # def state(self) -> State:
+        #     pass
+
+        def successors(self) -> Iterable["PartialSolution"]:
+            pass
+
+    initialps = PuzzlePS((), mapa, pos_jugador)
+    return BacktrackingOptSolver.solve(initialps)
+
+
 if __name__ == '__main__':
     datos = load_file()
-
-    a, b, c, d = read_level(datos)
-    print(datos)
-    # print(a, b, c, d)
+    if sys.argv[1] != "":
+        max_mov = sys.argv[1]
+    print(max_mov)
+    mapa, pos_jugador, lista_cajas_start, lista_cajas_end = read_level(datos)
+    print(mapa, pos_jugador, lista_cajas_start, lista_cajas_end)
+    print(mapa[0][5])
